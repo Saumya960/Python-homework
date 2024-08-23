1b65f32fcb74eb189c8f31101b0f9370fbc87d66
Saumya960 <saumyap.960602@gmail.com>
2024-08-23 11:19:45 +0300

    Initial commit

--- Task2.py
+++ Task2.py
@@ -12,3 +12,14 @@ area = length * width
 perimeter = 2 * (length + width)
 print(f"The are of the rectangle is: {area:.2f}")
 print(f"The perimeter of the rectangle is: {perimeter:.2f}")
+
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+num3 = int(input("Enter the third integer: "))
+sum_of_numbers = num1 + num2 + num3
+product_of_numbers = num1 * num2 * num3
+average_of_numbers = sum_of_numbers / 3
+print(f"The sum of {num1}, {num2},{num3} is: {sum_of_numbers}")
+print(f" The product of {num1}, {num2},{num3} is: {product_of_numbers}")
+print(f" The average of {num1}, {num2},{num3} is: {average_of_numbers}")
+
