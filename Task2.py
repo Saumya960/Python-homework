52e975a420abfc5c21c4d6b9ef0564d181744ac8
Saumya960 <saumyap.960602@gmail.com>
2024-08-23 10:25:39 +0300

    Initial commit

--- Task2.py
+++ Task2.py
@@ -5,3 +5,10 @@ import math
 radius = float(input("Enter the radius of the circle: "))
 area = math.pi * (radius ** 2)
 print(f"The area of thr circle {radius} is: {area: .2f}")
+
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+area = length * width
+perimeter = 2 * (length + width)
+print(f"The are of the rectangle is: {area:.2f}")
+print(f"The perimeter of the rectangle is: {perimeter:.2f}")
