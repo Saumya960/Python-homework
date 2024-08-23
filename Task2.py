a93f5cfc527e98a48081fa426afdcad4348f09b0
Saumya960 <saumyap.960602@gmail.com>
2024-08-23 10:00:48 +0300

    Initial commit

--- Task2.py
+++ Task2.py
@@ -1,2 +1,7 @@
 user = input('Enter your name: ')
 print("Nice to meet you, " + user + "!")
+
+import math
+radius = float(input("Enter the radius of the circle: "))
+area = math.pi * (radius ** 2)
+print(f"The area of thr circle {radius} is: {area: .2f}")
