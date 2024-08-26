1e886a1684895cd4dbea395d20a9794dacc05905
Saumya960 <saumyap.960602@gmail.com>
2024-08-26 11:54:52 +0300

    Initial commit

--- Module3/Task3.py
+++ Module3/Task3.py
@@ -7,6 +7,7 @@ else:
 below_limit = 42 - length
 print(f"The zander is {below_limit} centimeters below the size limit.")
 
+
 #Question2
 cabin_class = input("Enter the cabin class(LUX,A,B,C: ")
 if cabin_class == "LUX":
@@ -19,3 +20,28 @@ elif cabin_class == "C":
     print("C: windowless cabin below the car deck.")
 else :
     print("You entered an invalid cabin class")
+
+#Question3
+biological_gender = input("Enter the biological gender(adult_male,adult_female): ")
+hemoglobin_value = float(input("Enter the hemoglobin value: "))
+if biological_gender == "adult_female":
+    if 117 <= hemoglobin_value <= 155:
+        print("Hemoglobin value is normal")
+    elif hemoglobin_value < 117:
+        print("Hemoglobin value is law")
+    else:
+        print("Hemoglobin value is high")
+elif biological_gender == "adult female":
+    if 134 <= hemoglobin_value <= 167:
+        print("Hemoglobin value is normal")
+    elif hemoglobin_value < 134:
+        print("Hemoglobin value is law")
+    else:
+        print("Hemoglobin value is high")
+else:
+    print("You entered an invalid biological gender")
+
+
+
+
+
