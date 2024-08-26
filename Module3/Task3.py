dffec7db0a4c537010afe3fa7159c6c2c6bfbc84
Saumya960 <saumyap.960602@gmail.com>
2024-08-26 11:20:52 +0300

    Initial commit

--- Module3/Task3.py
+++ Module3/Task3.py
@@ -8,3 +8,14 @@ below_limit = 42 - length
 print(f"The zander is {below_limit} centimeters below the size limit.")
 
 #Question2
+cabin_class = input("Enter the cabin class(LUX,A,B,C: ")
+if cabin_class == "LUX":
+    print("LUX: upper-deck cabin with a balcony.")
+elif cabin_class == "A":
+    print ("A: above the car deck, equipped with a window.")
+elif cabin_class == "B":
+    print("B: windowless cabin above the car deck.")
+elif cabin_class == "C":
+    print("C: windowless cabin below the car deck.")
+else :
+    print("You entered an invalid cabin class")
