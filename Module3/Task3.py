e90e83d6b5feca75fa2baa667c8882ca80e5a313
Saumya960 <saumyap.960602@gmail.com>
2024-08-26 22:49:30 +0300

    Initial commit

--- Module3/Task3.py
+++ Module3/Task3.py
@@ -41,6 +41,16 @@ elif biological_gender == "male":
 else:
     print("You entered an invalid biological gender")
 
+#Question4
+year = int(input("Enter a year: "))
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(f"The input year is a leap year.")
+else:
+    print(f"The input year is not a leap year.")
+
+
+
+
 
 
 
