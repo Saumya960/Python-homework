d5ac6d7d8b88895893c15cf81c8198befa73df57
Saumya960 <saumyap.960602@gmail.com>
2024-08-30 11:44:58 +0300

    Initial commit

--- Module4/Task4.py
+++ Module4/Task4.py
@@ -1,5 +1,20 @@
+#Question1
 number = 1
 while number <= 1000:
     if number % 3 == 0:
         print(number)
     number += 1
+
+
+#Question2
+while True:
+    inches = int(input("Enter a value in inches: "))
+    if inches < 0:
+        print("Negative value entered")
+        break
+
+    centimeters = inches * 2.54
+    print("inches is equal to ", centimeters)
+
+
+
