5f9b8898f3ad224a81ceb6543c5f692a9aed368f
Saumya960 <saumyap.960602@gmail.com>
2024-09-03 14:36:50 +0300

    Initial commit

--- Module5/Task5.py
+++ Module5/Task5.py
@@ -26,4 +26,20 @@ for number in greatest_numbers:
 
 
 #Question3
+user_input = int(input("Enter an integer: "))
+prime_num = True
+if user_input <= 1:
+     prime_num = False
+else:
+     for i in range (2, user_input):
+          if user_input % i == 0:
+               prime_num = False
+               break
+
+if prime_num:
+     print(f"{user_input} is a prime number.")
+else:
+     print(f"{user_input} is not a prime number.")
+
+
 
