7a7e23cc62f46035635c0f8546b87708af89c1e4
Saumya960 <saumyap.960602@gmail.com>
2024-10-24 20:55:10 +0300

    Initial commit

--- Module9/Task1.py
+++ Module9/Task1.py
@@ -10,22 +10,45 @@ class Car:
         print(f"Maximum Speed: {self.max_speed} km/h")
         print(f"Current Speed: {self.current_speed} km/h")
         print(f"Travelled Distance: {self.travelled_distance} km")
-j
+
+    def accelerate(self, change_speed):
+        self.current_speed += change_speed
+
+        if self.current_speed > self.max_speed:
+            self.current_speed = self.max_speed
+
+        elif self.current_speed < 0:
+            self.current_speed = 0
+
+    def drive(self, hours):
+        self.travelled_distance += self.current_speed * hours
+
 def main():
     my_car = Car("ABC-123", 142)
     my_car.properties()
 
+    my_car.accelerate(30)
+    print(f"\nCurrent speed: {my_car.current_speed} km/h")
+
+    my_car.drive(1.5)
+    print(f"\nTravelled Distance: {my_car.travelled_distance} km")
+
+    my_car.accelerate(70)
+    print(f"\nCurrent speed: {my_car.current_speed} km/h")
+
+    my_car.drive(2)
+    print(f"\nTravelled Distance: {my_car.travelled_distance} km")
+
+    my_car.accelerate(50)
+    print(f"\nCurrent speed: {my_car.current_speed} km/h")
+
+    my_car.accelerate(-200)
+    print(f"\nFinal speed: {my_car.current_speed} km/h")
+
+    my_car.properties()
+
+
 main()
 
-ef accelerate(self, change_of_speed):
-        # Adjust the current speed with the provided change, ensuring it's within the valid range
-        self.current_speed += change_of_speed
 
-        # Make sure speed does not exceed the maximum speed
-        if self.current_speed > self.max_speed:
-            self.current_speed = self.max_speed
-
-        # Ensure speed does not fall below 0
-        elif self.current_speed < 0:
-            self.current_speed = 0
 
