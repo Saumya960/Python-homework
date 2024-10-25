646347ab810d1a57beb3d5196f2f52377d589677
Saumya960 <saumyap.960602@gmail.com>
2024-10-25 14:51:22 +0300

    Initial commit

--- Module9/task4.py
+++ Module9/task4.py
@@ -1,15 +1,12 @@
-
 import random
 from car import Car
 
-# Initialize 10 cars with random maximum speeds
 cars = []
 for i in range(10):
     registration_number = f"ABC-{ i +1}"
-    max_speed = random.randint(100, 200)  # Random maximum speed between 100 and 200 km/h
+    max_speed = random.randint(100, 200)
     cars.append(Car(registration_number, max_speed))
 
-# Race simulation
 race_ongoing = True
 while race_ongoing:
     for car in cars:
@@ -22,7 +19,6 @@ while race_ongoing:
             race_ongoing = False
             break
 
-# Display the final race results
 print \
     (f"{'Registration': <12} | {'Max Speed (km/h)': <15} | {'Current Speed (km/h)': <20} | {'Travelled Distance (km)'}")
 print("-" * 65)
